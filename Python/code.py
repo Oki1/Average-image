ac341727f88a17a4ebf8367c6d3aa5b134e26c45
Oki1 <oki.archer@gmail.com>
2019-02-05 12:42:56 +0100

    Done v1.0
    The script works

--- Python/code.py
+++ Python/code.py
@@ -1,60 +1,61 @@
 from PIL import Image
 from math import fabs
-import os
-blurry = r"Images/man.jpeg"
-im_blurry = Image.open(blurry)
-size = (100,100)
-clear = lambda: os.system('cls')
 
-def returnAverage(one, two):
 
-    averager = (one[0] + two[0]) / 2
-    averageg = (one[1] + two[1]) / 2
-    averageb = (one[2] + two[2]) / 2
-    return(round(averager), round(averageg), round(averageb))
+imageName = "matise.jfif"
+threshold = 20
 
-def show(image):
-    image.show()
 
-def similar(one, two, num = 20):
-    if(round(fabs(one[0] - two[0])) >= num):
-        return(False)
-    if(round(fabs(one[1] - two[1])) >= num):
-        return(False)
-    if(round(fabs(one[1] - two[1])) >= num):
-        return(False)
-    return (True)
 
-def BiggyfyColor(size, color): #dat spelling
+image = Image.open("Images/"+imageName)
+size = image.size
+data = list(image.getdata()) 
+#AVERAGES---DONE                     CODE
+def similar( color1,color2,threshold = 20):
+    for x in range(0,3):
+        if(not fabs(color1[x] - color2[x]) <= threshold):
+            return False
+    return True
+
+def average_func(colors1,colors2):
+    ret = []
+    def yld():
+        x = zip(colors1,colors2)
+        for y in x:
+            yield int(sum(y)/2)
+    for i in yld():
+        ret.append(i)
+    return(tuple(ret))
+
+def giveData(data, num = 20):
+    averages = []
+    code = []
+    def generator_data():
+        for pix in data:
+            yield pix
+
+    gen_data = generator_data()
+    for pix in gen_data:
+        foundMate = False
+        for x in range(0,len(averages)):
+            if(similar(pix, averages[x], threshold=num)):  #IF SIMILAR
+                averages[x] = average_func(pix, averages[x]) #AVERAGE
+                code.append(x)
+                foundMate = True
+                break
+        if(not foundMate):  #IF NO SIMILAR AVERAGE WAS FOUND
+            foundMate = True
+            averages.append(pix)
+            code.append(averages.index(averages[-1]))
+    return([code,averages])
+
+def makeImage(code, averages):
+    im = Image.new("RGB", size)
     data = []
-    for x in range(0, size[0]*size[1]):
-        data.append(color)
-    return(tuple(data))
+    for i in code:
+        data.append(averages[i])
+    im.putdata(data)
+    im.show()
 
-def showColor():
-    color = returnAverage(list(im_blurry.getdata()))
-    averageNew  = Image.new("RGB", size)
-    averageNew.putdata(BiggyfyColor(size, color))
-    averageNew.show();
-
-def getTeams(image, similarity):
-
-    teams = [[0]]
-    data = image.getdata()
-    teamsAverage = [data[0]]
-
-    size = image.size
-    print(len(data))
-    for z in range(0, len(data)):
-        pixel = data[z]
-        added = False
-        for x in range(0, len(teams)):
-            if(similar(teamsAverage[x], pixel, similarity)):
-                teams[x].append(z)
-                teamsAverage[x] = returnAverage(teamsAverage[x], pixel)
-                added = True
-        if(not added):
-            teams.append([z])
-            teamsAverage.append(pixel)
-    print(len(teams))
-getTeams(im_blurry, 20)
+dat = giveData(data,threshold)
+makeImage(dat[0], dat[1])
